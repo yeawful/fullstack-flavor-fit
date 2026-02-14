a71609f25f858fa1b87eae56e38a427805c80306
yeawful <paketa1337@gmail.com>
2026-02-14 15:47:06 +0300

    fix graphql-codegen & apollo v4; add base login/register form

--- front-end-flavor-fit/src/__generated__/graphql.ts
+++ front-end-flavor-fit/src/__generated__/graphql.ts
@@ -1,13 +1,12 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
+/* eslint-disable */
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
-export type InputMaybe<T> = Maybe<T>;
+export type InputMaybe<T> = T | null | undefined;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
 export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
-const defaultOptions = {} as const;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: { input: string; output: string; }
@@ -15,6 +14,7 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
+  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
   DateTime: { input: any; output: any; }
 };
 
@@ -441,40 +441,13 @@ export type LoginMutationVariables = Exact<{
 
 export type LoginMutation = { __typename?: 'Mutation', login: { __typename?: 'AuthResponse', accessToken: string, user: { __typename?: 'UserModel', email: string } } };
 
+export type RegisterMutationVariables = Exact<{
+  data: AuthInput;
+}>;
 
-export const LoginDocument = gql`
-    mutation Login($data: AuthInput!) {
-  login(data: $data) {
-    accessToken
-    user {
-      email
-    }
-  }
-}
-    `;
-export type LoginMutationFn = Apollo.MutationFunction<LoginMutation, LoginMutationVariables>;
 
-/**
- * __useLoginMutation__
- *
- * To run a mutation, you first call `useLoginMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useLoginMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [loginMutation, { data, loading, error }] = useLoginMutation({
- *   variables: {
- *      data: // value for 'data'
- *   },
- * });
- */
-export function useLoginMutation(baseOptions?: Apollo.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, options);
-      }
-export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
-export type LoginMutationResult = Apollo.MutationResult<LoginMutation>;
-export type LoginMutationOptions = Apollo.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
+export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'AuthResponse', accessToken: string, user: { __typename?: 'UserModel', email: string } } };
+
+
+export const LoginDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"Login"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"data"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"AuthInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"login"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"data"},"value":{"kind":"Variable","name":{"kind":"Name","value":"data"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"accessToken"}},{"kind":"Field","name":{"kind":"Name","value":"user"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"email"}}]}}]}}]}}]} as unknown as DocumentNode<LoginMutation, LoginMutationVariables>;
+export const RegisterDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"mutation","name":{"kind":"Name","value":"Register"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"data"}},"type":{"kind":"NonNullType","type":{"kind":"NamedType","name":{"kind":"Name","value":"AuthInput"}}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"register"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"data"},"value":{"kind":"Variable","name":{"kind":"Name","value":"data"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"accessToken"}},{"kind":"Field","name":{"kind":"Name","value":"user"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"email"}}]}}]}}]}}]} as unknown as DocumentNode<RegisterMutation, RegisterMutationVariables>;
--- front-end-flavor-fit/src/features/auth/ui/AuthForm.tsx
+++ front-end-flavor-fit/src/features/auth/ui/AuthForm.tsx
@@ -1,24 +1,73 @@
 'use client'
 
+import { Button } from '@/shared/components/ui/button'
+import { Input } from '@/shared/components/ui/input'
+import { useMutation } from '@apollo/client/react'
+import Link from 'next/link'
+
+import { PAGES } from '@/shared/config/page.config'
+
+import { LoginDocument, RegisterDocument } from '@/__generated__/graphql'
+
 interface Props {
   type: 'login' | 'register'
 }
 
 export function AuthForm({ type }: Props) {
+  const isLogin = type === 'login'
+  const [register, { data, loading, error }] = useMutation(
+    isLogin ? LoginDocument : RegisterDocument
+  )
+
   return (
-    <div>
-      <h1>{type === 'register' ? 'Register' : 'Login'}</h1>
+    <div className="flex h-screen">
+      <div className="m-auto w-sm rounded-lg bg-linear-to-tr from-[#8062ee] to-[#a088fc] p-5 text-white shadow-lg">
+        <h1 className="mb-5 text-center text-4xl font-bold">
+          {isLogin ? 'Login' : 'Register'}
+        </h1>
 
-      <form>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-        />
+        <form className="space-y-3">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
 
-        <button>{type === 'register' ? 'Register' : 'Login'}</button>
-      </form>
+          <div className="text-center">
+            <Button
+              type="submit"
+              disabled={loading}
+            >
+              {isLogin ? 'Login' : 'Register'}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-3 text-center">
+          {isLogin ? (
+            <div>
+              Don&apos;t have an account?{' '}
+              <Link
+                href={PAGES.REGISTER}
+                className="underline"
+              >
+                Register
+              </Link>
+            </div>
+          ) : (
+            <div>
+              Already have an account?{' '}
+              <Link
+                href={PAGES.LOGIN}
+                className="underline"
+              >
+                Login
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
